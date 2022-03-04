f69ecd06a12377a352f22bf1c67f6f09a5837746
shanta4 <100867138+shanta4@users.noreply.github.com>
2022-03-04 11:57:14 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>e-School</title>
+ <style>
+.menubar{
+	background-color:#40868b;
+	padding:1% 7%;
+	text-align: center;
+	}
+.menubar a{
+    align:right;
+	color:white;
+	text-decoration:none;
+	font-size:15px;
+	padding:10px 20px;
+	cursor:pointer;
+    }	
+
+
+</style>
+</head>
+
+<body bgcolor="#A6ACAF">
+
+<div class="menubar">
+
+ <img src="Logo.png" height="50px" width="80px" >
+<a href="signin.php">Sign In</a>
+<a href="blog.php"> Blog</a>
+<a href="others.php">Others</a>
+<a href="courses.php">Courses</a>
+<a href="books.php">Books</a>
+</div>
+
+
+ <div bg color="black">
+ <p> <font color="red">Are you ready to Learn? </font></p> 
+ <font><h1>Learn With Fun  </h1> </font>
+ on <font color="green" > <h1> any schedule </h1>   </font>
+ <p>Lorem ipsum dolor sit amet,consectetur<br>
+	 adipiscing elit various blandit facillisis quam netus
+ </p>
+  
+
+ <img src="Illustration.png" height="300px" width="300px" >
+</div>
+ 	
+					
+					
+ 
+
+</body>
+</html>
